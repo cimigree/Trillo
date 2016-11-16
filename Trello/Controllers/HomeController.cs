bc38b9f13671e3cfe424df2eee20b62c590b9a8b
Cindy Green <cimigreen@hotmail.com>
2016-11-16 16:57:20 -0500

    Trello draggable

---
+++ Trello/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trello.Models;
+
+namespace Trello.Controllers
+{
+    public class HomeController : Controller
+    {
+        // This establishes a connection to the Database.
+        private TrelloDbContext db = new TrelloDbContext();
+
+        // This action creates our home view, which will display all of the taskcards.
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //This action (function/method) will be called to create a new card.
+        public ActionResult CreateTask()
+        {
+            return View();
+        }
+
+        // Displaying users task individually (when clicked on and after new one is created or edited)
+        public ActionResult ViewTask(int ID)
+        {
+            Trillo myTrello = db.Trillos.Find(ID);
+            ViewBag.Message = "Your contact page.";
+            return View(myTrello);
+
+        }
+
+        // Finding a task by ID and bringing it up to edit
+
+        public ActionResult EditTask(int ID)
+        {
+            Trillo myTrello = db.Trillos.Find(ID);
+            return View(myTrello);
+
+        }
+
+        //Save a task after it has been edited and updated and then display the new task.
+
+        public ActionResult UpdateTask(int ID, string TaskName, string Notes, DateTime DueDate)
+        {
+            Trillo myTrello = db.Trillos.Find(ID);
+            myTrello.TaskName = TaskName;
+            myTrello.Notes = Notes;
+            myTrello.DueDate = DueDate;       
+            db.SaveChanges();
+            return RedirectToAction("ViewTask", new { ID = myTrello.ID });
+
+        }
+        
+        // Displaying all of the Trello cards
+        public ActionResult DisplayTasks()
+        {
+            return View(db.Trillos.ToList());
+        }
+
+        //Finding correct task by ID, removing it, displaying the homepage with all of the tasks
+
+        public ActionResult Delete(int ID)
+        {
+            Trillo myTrello = db.Trillos.Find(ID);
+            db.Trillos.Remove(myTrello);
+            return RedirectToAction("DisplayTasks");
+
+        }
+
+        //This happens when save is clicked. Takes in the CreateTask and saves it to the database and then displays the task.
+        public ActionResult SaveTask(string TaskName, string Notes, DateTime DueDate)
+        {
+            Trillo myTrello = new Trillo();
+            myTrello.TaskName = TaskName;
+            myTrello.Notes = Notes;
+            myTrello.DueDate = DueDate;
+            myTrello.SortOrder = myTrello.ID;
+
+            db.Trillos.Add(myTrello);
+            db.SaveChanges();
+
+            return RedirectToAction("ViewTask", new { id = myTrello.ID });
+
+
+
+
+
+
+        }
+
+
+       
+
+     
+
+        
+    }
+}
---
+++ Trello/Models/Trello.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Trello.Models
+{
+   // Talks to SQL and declares the variable type and column name
+    public class Trillo
+    {
+        public int ID { get; set; }
+        public string TaskName { get; set; }
+        public string Notes { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:-MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DueDate { get; set; }
+        public int SortOrder { get; set; }
+    }
+
+    // connects the model to the database
+
+    public class TrelloDbContext : DbContext
+    {
+        public DbSet<Trillo> Trillos { get; set; }
+    }
+}
